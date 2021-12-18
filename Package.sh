ae734f1245380e7fcd00087e480940934c48458b
chris1111 <leblond1111@gmail.com>
2021-12-18 09:24:51 -0500

    Make Package.sh exe

---
+++ Package.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# (c) Copyright 2021 chris1111
+PARENTDIR=$(dirname "$0")
+cd "$PARENTDIR"
+echo "Build the Project"
+Sleep 2
+xcodebuild -sdk macosx -configuration Release
+# Create the Packages with pkgbuild/productbuild
+echo "Build the Package"
+Sleep 2
+cp -r ./build/Release/HoRNDIS.kext ./Package/HoRNDIS
+sleep 1
+pkgbuild --root ./Package/HoRNDIS --scripts ./Package/scripts --identifier com.horndis.horndis --version 1 --install-location /Library/Extensions HoRNDIS.pkg
+sleep 1
+productbuild --distribution  ./Package/Distribution.xml --resources ./Package/Resources --package-path ./HoRNDIS.pkg ./Build/HoRNDIS-Package.pkg
+rm -rf ./HoRNDIS.pkg
+Open ./build
+echo "** PACKAGE BUILD SUCCEEDED **"
+
